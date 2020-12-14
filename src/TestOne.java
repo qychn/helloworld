2d54789a52641fcd2d6c74b8deb897cbc8bbe344
QY <xiaadadad@qq.com>
2020-12-14 11:18:33 +0800

    b

--- src/TestOne.java
+++ src/TestOne.java
@@ -2,4 +2,8 @@ public class TestOne {
     public static void main(String[] args) {
         System.out.println("helleoworld");
     }
+
+    private void test2(){
+        System.out.println("{");
+    }
 }
