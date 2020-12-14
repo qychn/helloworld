d1069c6784b842483adff36a5c66f17a5f4fe8a6
QY <xiaadadad@qq.com>
2020-12-14 10:56:42 +0800

    fenzhi 1

--- src/TestTwo.java
+++ src/TestTwo.java
@@ -2,4 +2,8 @@ public class TestTwo {
     public static void main(String[] args) {
         System.out.println("dierci");
     }
+
+    private void test(){
+        System.out.println("branch1");
+    }
 }
