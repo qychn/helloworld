2d43e48ee009e2c2427ac4c2d0595d99d84b64c7
QY <xiaadadad@qq.com>
2020-12-14 11:10:43 +0800

    reset

--- src/TestTwo.java
+++ src/TestTwo.java
@@ -2,4 +2,9 @@ public class TestTwo {
     public static void main(String[] args) {
         System.out.println("dierci");
     }
+
+
+    private void test3(){
+        System.out.println("reset");
+    }
 }
